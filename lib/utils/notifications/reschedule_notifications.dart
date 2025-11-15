0914cc245fab472c3863438860ce2f27b76a05d7
Mounir Nessim <mmnessim@gmail.com>
2025-11-15 10:38:51 -0500

    Move notifyNotificationReschedule from clearAndRescheduleNotifications to scheduleNotificationWithId so it is always called

--- lib/utils/notifications/reschedule_notifications.dart
+++ lib/utils/notifications/reschedule_notifications.dart
@@ -62,7 +62,7 @@ Future<void> clearAndRescheduleNotifications({
         await loadNotificationTime() ?? TimeOfDay.now(),
       ),
     );
-    notifyNotificationReschedule(terms.indexOf(term));
+    // notifyNotificationReschedule(terms.indexOf(term));
     await scheduleNotificationWithId(spec, _plugin);
   }
 }
