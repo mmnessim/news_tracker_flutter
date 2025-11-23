c0cd90b16e93bd5473553340a0c2b1b15c3d7b88
Mounir Nessim <mmnessim@gmail.com>
2025-11-23 11:26:10 -0500

    call clearAndRescheduleNotifications() in _setNotificationTime

--- lib/widgets/time_picker_row.dart
+++ lib/widgets/time_picker_row.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_tracker/providers/notification_time_provider.dart';
 import 'package:news_tracker/utils/notifications/reschedule_notifications.dart';
-import 'package:news_tracker/utils/preferences.dart';
 
 class TimePickerRow extends ConsumerWidget {
   void _setNotificationTime(
@@ -17,7 +16,7 @@ class TimePickerRow extends ConsumerWidget {
     if (picked != null) {
       ref.read(notificationTimeProvider.notifier).setNewTime(picked);
     }
-    //clearAndRescheduleNotifications();
+    clearAndRescheduleNotifications();
   }
 
   @override
@@ -57,68 +56,3 @@ class TimePickerRow extends ConsumerWidget {
     );
   }
 }
-
-// class _TimePickerRow extends StatefulWidget {
-//   const _TimePickerRow({super.key, this.notificationTime});
-//
-//   final TimeOfDay? notificationTime;
-//
-//   @override
-//   State<_TimePickerRow> createState() => _TimePickerRowState();
-// }
-//
-// class _TimePickerRowState extends State<_TimePickerRow> {
-//   TimeOfDay _selectedTime = TimeOfDay.now();
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _loadInitialTime();
-//   }
-//
-//   Future<void> _loadInitialTime() async {
-//     final loadedTime = await loadNotificationTime();
-//     setState(() {
-//       _selectedTime = loadedTime ?? TimeOfDay.now();
-//     });
-//   }
-//
-//   void _setNotificationTime() async {
-//     final picked = await showTimePicker(
-//       context: context,
-//       initialTime: _selectedTime,
-//     );
-//     if (picked != null) {
-//       setState(() {
-//         _selectedTime = picked;
-//       });
-//       await saveNotificationTime(picked);
-//     }
-//     clearAndRescheduleNotifications();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Theme.of(context).colorScheme.primary,
-//       padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-//       child: InkWell(
-//         onTap: _setNotificationTime,
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             Expanded(
-//               child: Text(
-//                 'Notification time: ${_selectedTime.format(context)}',
-//                 style: TextStyle(color: Colors.white),
-//                 softWrap: true,
-//                 overflow: TextOverflow.visible,
-//               ),
-//             ),
-//             Icon(Icons.access_time_outlined, color: Colors.white),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
