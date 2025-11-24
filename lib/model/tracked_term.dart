93a71cc97933925677d106417d655e5a3ec3fdfe
Mounir Nessim <mmnessim@gmail.com>
2025-11-24 05:51:14 -0500

    Add tracked term model

---
+++ lib/model/tracked_term.dart
@@ -0,0 +1,36 @@
+class TrackedTerm {
+  final String term;
+  final int id;
+  bool locked;
+  String? lastArticleHash;
+
+  TrackedTerm({
+    required this.term,
+    required this.id,
+    this.locked = false,
+    this.lastArticleHash,
+  });
+
+  TrackedTerm copyWith({
+    String? term,
+    int? id,
+    bool? locked,
+    String? lastArticleHash,
+  }) {
+    return TrackedTerm(
+      term: term ?? this.term,
+      id: id ?? this.id,
+      locked: locked ?? this.locked,
+      lastArticleHash: lastArticleHash ?? this.lastArticleHash,
+    );
+  }
+
+  factory TrackedTerm.fromJson(Map<String, dynamic> json) {
+    return TrackedTerm(
+      term: json['term'],
+      id: json['id'],
+      locked: json['locked'] ?? false,
+      lastArticleHash: json['lastArticleHash'],
+    );
+  }
+}
